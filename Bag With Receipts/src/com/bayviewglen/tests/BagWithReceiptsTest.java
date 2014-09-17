57ce85e0c457406563bea0be22343ce7ecc5969b
pnadjafov <pnadjafov@bayviewglen.ca>
2014-09-17 19:33:32 -0400

    Completed "remove" JUnit test.

--- Bag With Receipts/src/com/bayviewglen/tests/BagWithReceiptsTest.java
+++ Bag With Receipts/src/com/bayviewglen/tests/BagWithReceiptsTest.java
@@ -22,14 +22,14 @@ public class BagWithReceiptsTest {
 		b1.add(3);
 		b1.add(3);
 		assertEquals("Occurences, Integer test:", 3, b1.countOccurrences(3));
-		
+
 		b1 = new BagWithReceipts();
 		b1.add(3);
 		b1.add("Hi");
 		b1.add(3);
 		b1.add(3);
 		assertEquals("Occurences, zero test:", 0, b1.countOccurrences("Hello"));
-		
+
 		b1 = new BagWithReceipts();
 		b1.add(3);
 		b1.add("Hi");
@@ -38,5 +38,25 @@ public class BagWithReceiptsTest {
 		String s = null;
 		assertEquals("Occurences, null test:", 0, b1.countOccurrences(s));
 	}
-
+	
+	@Test
+	public void testRemove() {
+		BagWithReceipts bag = new BagWithReceipts();
+		bag.add("Test1");
+		bag.add("Test2");
+		bag.add("Test3");
+		assertEquals("Remove, String test:",true,bag.remove("Test2"));
+		
+		bag = new BagWithReceipts();
+		bag.add(1);
+		bag.add("Test");
+		bag.add(2);
+		assertEquals("Remove, Integer test:",true,bag.remove(2));
+		
+		bag = new BagWithReceipts();
+		bag.add(1);
+		bag.add("Test");
+		bag.add(2);
+		assertEquals("Remove, false test:",false,bag.remove(3));
+	}
 }
